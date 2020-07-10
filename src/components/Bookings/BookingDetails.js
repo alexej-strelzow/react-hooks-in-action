dc59eb31a2d3f8f5c28385528c3f92e4e3b3f714
John Larsen <jr.larsen@mac.com>
2020-07-10 11:38:36 +0100

    Update from placeholder to showing booking details

--- src/components/Bookings/BookingDetails.js
+++ src/components/Bookings/BookingDetails.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, {Fragment} from "react";
 
-export default function BookingDetails () {
+function Booking ({booking, bookable}) {
+  const {title, date, session, notes} = booking;
+
   return (
-    <div className="booking-details placeholder">
-      <h3>Booking Details</h3>
+    <div className="booking-details-fields">
+      <label>Title</label>
+      <p>{title}</p>
+
+      <label>Bookable</label>
+      <p>{bookable.title}</p>
+
+      <label>Booking Date</label>
+      <p>{(new Date(date)).toDateString()}</p>
+
+      <label>Session</label>
+      <p>{session}</p>
+
+      {notes && (
+        <Fragment>
+          <label>Notes</label>
+          <p>{notes}</p>
+        </Fragment>
+      )}
+    </div>
+  )
+}
+
+export default function BookingDetails ({booking, bookable}) {
+  return (
+    <div className="booking-details">
+      <h2>Booking Details</h2>
+
+      {booking ? (
+        <Booking
+          booking={booking}
+          bookable={bookable}
+        />
+      ) : (
+        <div className="booking-details-fields">
+          <p>Select a booking or a booking slot.</p>
+        </div>
+      )}
     </div>
   );
 }
