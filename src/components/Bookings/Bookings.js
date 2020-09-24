9a7a37fa32ddfaf4e44edcab3dedd3191cc607fa
John Larsen <jr.larsen@mac.com>
2020-09-24 21:33:00 +0100

    slide the booking grid down with React Spring

--- src/components/Bookings/Bookings.js
+++ src/components/Bookings/Bookings.js
@@ -4,7 +4,7 @@ import {getWeek, shortISO} from "../../utils/date-wrangler";
 import {useBookingsParams, useBookings} from "./bookingsHooks";
 
 import WeekPicker from "./WeekPicker";
-import BookingsGrid from "./BookingsGrid";
+import BookingsGridSlide from "./BookingsGridSlide";
 import BookingDetails from "./BookingDetails";
 
 export default function Bookings ({bookable}) {
@@ -21,12 +21,18 @@ export default function Bookings ({bookable}) {
     setBooking(null);
   }, [bookable, weekStart]);
 
+  useEffect(() => {
+    if (booking?.id !== undefined && !selectedBooking) {
+      setBooking(null);
+    }
+  }, [booking, selectedBooking]);
+
   return (
     <div className="bookings">
       <div>
         <WeekPicker/>
 
-        <BookingsGrid
+        <BookingsGridSlide
           week={week}
           bookable={bookable}
           booking={booking}
