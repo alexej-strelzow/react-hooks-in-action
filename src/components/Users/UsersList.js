4f572186ecc0fc06bffff4a0a5d12a96bc630d24
John Larsen <jr.larsen@mac.com>
2020-10-04 11:17:56 +0100

    create separate UserDetails component with user avatar

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,54 +1,29 @@
-import React, {useState, Fragment} from 'react';
+import React from 'react';
 import {useQuery} from "react-query";
-
-import {useUser} from "../../contexts/UserContext";
 import getData from "../../utils/api";
 
-export default function UsersList () {
-  const [loggedInUser] = useUser();
-
-  const {data: users = []} = useQuery(
+export default function UsersList ({user, setUser}) {
+  const {data: users} = useQuery(
     "users",
     () => getData("http://localhost:3001/users"),
-    {
-      suspense: true
-    }
+    {suspense: true}
   );
 
-  const [selectedUser, setSelectedUser] = useState(null);
-  const user = selectedUser || loggedInUser;
-
   return (
-    <Fragment>
-      <ul className="users items-list-nav">
-        {users.map(u => (
-          <li
-            key={u.title}
-            className={u.id === user?.id ? "selected" : null}
+    <ul className="users items-list-nav">
+      {users.map(u => (
+        <li
+          key={u.title}
+          className={u.id === user?.id ? "selected" : null}
+        >
+          <button
+            className="btn"
+            onClick={() => setUser(u)}
           >
-            <button
-              className="btn"
-              onClick={() => {
-                setSelectedUser(u);
-              }}
-            >
-              {u.name}
-            </button>
-          </li>
-        ))}
-      </ul>
-
-      {user && (
-        <div className="item user">
-          <div className="item-header">
-            <h2>{user.name}</h2>
-          </div>
-          <div className="user-details">
-            <h3>{user.title}</h3>
-            <p>{user.notes}</p>
-          </div>
-        </div>
-      )}
-    </Fragment>
+            {u.name}
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 }
--- src/components/Users/UsersPage.js
+++ src/components/Users/UsersPage.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, {useState, Suspense} from "react";
 import UsersList from "./UsersList";
+import {useUser} from "../../contexts/UserContext";
+import PageSpinner from "../UI/PageSpinner";
+import UserDetails from "./UserDetails";
 
 export default function UsersPage() {
-  return (
+  const [loggedInUser] = useUser();
+  const [selectedUser, setSelectedUser] = useState(null);
+  const user = selectedUser || loggedInUser;
+
+  function switchUser(nextUser) {
+    setSelectedUser(nextUser);
+  }
+
+  return user ? (
     <main className="users-page">
-      <UsersList/>
+      <UsersList user={user} setUser={switchUser}/>
+
+      <Suspense fallback={<PageSpinner/>}>
+        <UserDetails userID={user.id}/>
+      </Suspense>
     </main>
-  );
+  ) : null;
 }
