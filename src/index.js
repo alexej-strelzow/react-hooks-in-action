8dc9abc1cd525674d7ef8b616d150c5ea4f2ea7f
John Larsen <jr.larsen@mac.com>
2020-10-08 16:54:30 +0100

    format index.js to match book

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App.js';
 
 const root = document.getElementById('root');
-ReactDOM.unstable_createRoot(root).render(<App />);
+ReactDOM
+  .unstable_createRoot(root)
+  .render(<App />);
